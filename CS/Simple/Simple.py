3a370fe369cab3006efbe61def537a75612fd1cf
Kent Richardson <tkentrich@gmail.com>
2017-01-18 21:30:21 -0500

    Notify start

--- CS/Simple/Simple.py
+++ CS/Simple/Simple.py
@@ -3,7 +3,7 @@
 class CannotJoin(Exception):
 	pass
 
-class Game:
+class Game(object):
 	CREATED = 100
 	FILLED = 101
 	STARTING = 200
@@ -12,8 +12,8 @@ class Game:
 	COMPLETE = 300
 
 	def __init__(self, creator, scene):
-		self.players = [LevelObject(creator)]
-		self.objects = [players[-1]]
+		self.players = [Player(creator)]
+		self.objects = [self.players[-1]]
 		self.scene = scene
 		# This should depend on the scene
 		self.minPlayers = 1
@@ -22,8 +22,8 @@ class Game:
 
 	def join(self, joiner):
 		if self.status < Game.FILLED:
-			self.players.append(LevelObject(joiner))
-			self.addObject(players[-1])
+			self.players.append(Player(joiner))
+			self.addObject(self.players[-1])
 			if len(self.players) == self.maxPlayers:
 				self.status = Game.FILLED
 		else:
@@ -39,6 +39,10 @@ class Game:
 
 			self.status = Game.STARTING
 
+	def startGame(self):
+		#Set positions (unless already done...hmm...)
+		self.status = Game.STARTED
+
 	def addObject(self, obj):
 		self.objects.append(obj)
 
@@ -48,13 +52,21 @@ class Game:
 	def desc(self):
 		return "%s|%d/%d/%d" % (self.name(), len(self.players), self.minPlayers, self.maxPlayers)
 
-	def getUpdate(self):
+	def getUpdate(self, asker=None):
 		if self.status < Game.STARTED:
-			return name() + "^".join([p.name for p in self.players]) 
+			if self.status == Game.STARTING and asker != None:
+				self.players[asker].notifyStart = True
+				allNotified = True
+				for p in self.players:
+					 if not p.notifyStart:
+						allNotified = False
+				if allNotified:
+					self.startGame()
+			return self.name() + "^".join([p.name for p in self.players]) 
 		elif self.status < Game.COMPLETE:
 			return "^".join([p.desc() for p in self.players])
 
-class LevelObject:
+class LevelObject(object):
 	def __init__(self, name):
 		self.name = name
 		self.setPosition(0,0,0)
@@ -68,3 +80,7 @@ class LevelObject:
 	def desc(self):
 		return "%s|(%d,%d,%d)" % (self.name, self.x, self.y, self.z)
 
+class Player(LevelObject):
+	def __init__(self, name):
+		LevelObject.__init__(self, name)
+		self.notifiedStart = False
